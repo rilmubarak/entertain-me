76874524726ccf090f731f8cdb5e9800a0bb3a61
mubaraqhere <mubaraqhere@gmail.com>
2020-09-17 23:27:46 +0700

    AWS Deploy update: + config movies & tvSeries configuration + added some package

--- server/services/movies/config/index.js
+++ server/services/movies/config/index.js
@@ -1,10 +1,27 @@
-const MongoClient = require('mongodb').MongoClient;
-const dbUrl = 'mongodb://localhost:27017';
-const dbName = 'entertainMeMovie';
+// const MongoClient = require('mongodb').MongoClient;
+// const dbUrl = 'mongodb://localhost:27017';
+// const dbName = 'entertainMeMovie';
 
-const client = new MongoClient(dbUrl, {useUnifiedTopology: true})
-client.connect()
+// const client = new MongoClient(dbUrl, {useUnifiedTopology: true})
+// client.connect()
 
-const db = client.db(dbName)
+// const db = client.db(dbName)
 
-module.exports = db
+// module.exports = db
+
+// const MongoURI = 'mongodb+srv://entertainMeMovie:mongoDB@cluster0.t0ath.mongodb.net/entertainMeMovie?retryWrites=true&w=majority'
+
+const MongoURI = process.env.MONGO_SERVER_URI || 'mongodb://localhost:27017';
+if (process.env.MONGO_SERVER_URI) {
+    const mongoose = require('mongoose');
+    mongoose.Promise = global.Promise;
+    const db = mongoose.createConnection(MongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    module.exports = db;
+} else {
+    const MongoClient = require('mongodb').MongoClient;
+    const db_name = 'entertainMeMovie';
+    const client = new MongoClient(MongoURI, { useUnifiedTopology: true });
+    client.connect();
+    const db = client.db(db_name);
+    module.exports = db;
+}
--- server/services/tvSeries/config/index.js
+++ server/services/tvSeries/config/index.js
@@ -1,10 +1,25 @@
-const MongoClient = require('mongodb').MongoClient;
-const dbUrl = 'mongodb://localhost:27017';
-const dbName = 'entertainMeSerie';
+// const MongoClient = require('mongodb').MongoClient;
+// const dbUrl = 'mongodb://localhost:27017';
+// const dbName = 'entertainMeSerie';
 
-const client = new MongoClient(dbUrl, {useUnifiedTopology: true})
-client.connect()
+// const client = new MongoClient(dbUrl, {useUnifiedTopology: true})
+// client.connect()
 
-const db = client.db(dbName)
+// const db = client.db(dbName)
 
-module.exports = db
+// module.exports = db
+
+const MongoURI = process.env.MONGO_SERVER_URI || 'mongodb://localhost:27017';
+if (process.env.MONGO_SERVER_URI) {
+    const mongoose = require('mongoose');
+    mongoose.Promise = global.Promise;
+    const db = mongoose.createConnection(MongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    module.exports = db;
+} else {
+    const MongoClient = require('mongodb').MongoClient;
+    const db_name = 'entertainMeSerie';
+    const client = new MongoClient(MongoURI, { useUnifiedTopology: true });
+    client.connect();
+    const db = client.db(db_name);
+    module.exports = db;
+}
